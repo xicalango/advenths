e75e76ee83cfef6cf54b1d56a73faa27e3ea6c8d
Alexander Weld/celebex <alex.weld@gmx.net>
2009-11-27 18:46:03 +0100

    pickup/drop/look at & exits->M.Map

--- advent2.hs
+++ advent2.hs
@@ -11,21 +11,21 @@ data Dir =
 	| East
 	| South
 	| West
-	deriving (Eq,Show)
+	deriving (Ord,Eq,Show)
 
 data Action =
 	  Goto Dir
 	| Quit
-	| Look --Maybe String
+	| Look (Maybe String)
 	| Inventory
-	| Pickup --String
-	| Drop --String
+	| Pickup String
+	| Drop String
 	| NAA String --Not an Action
 
 data Room = Room { rID :: String
                  , rTitle :: String
                  , rDesc :: String
-                 , rExits :: [Exit]
+                 , rExits :: M.Map Dir String
                  , rVisited :: Bool
                  , rItems :: S.Set String
                  }
@@ -42,28 +42,45 @@ data GameState = GameState { gsRooms :: M.Map String Room
                            , gsInventory :: S.Set String
                            }
 
-type Exit = (Dir,String)
-
 items = [Item { iID = "cage"
               , iPre = "a little"
-              , iName = "Cage"
-              , iDesc = "A little cage"
+              , iName = "cage"
+              , iDesc = "A little cage."
+              }
+        ,Item { iID = "rod"
+              , iPre = "a"
+              , iName = "rod"
+              , iDesc = "A black and shiny rod."
               }
         ]
 
 rooms = [Room { rID = "main"
               , rTitle = "Mainway"
               , rDesc = "This is the mainway.\nThere is a small passage to the north."
-              , rExits = [(North,"back")]
+              , rExits = M.fromList [(North,"back")]
               , rVisited = False 
               , rItems = S.fromList ["cage"]
               }
         ,Room { rID = "back"
               , rTitle = "Backway"
-              , rDesc = "This is the backway.\nThere is a big passage to the south."
-              , rExits = [(South,"main")]
+              , rDesc = "This is the backway.\nThere is a big passage to the south.\nAlso you can go east an west."
+              , rExits = M.fromList [(South,"main"),(East,"beast"),(West,"best")]
               , rVisited = False
               , rItems = S.empty 
+              }
+        ,Room { rID = "beast"
+              , rTitle = "Back East"
+              , rDesc = "You have come to the beast!"
+              , rExits = M.fromList [(South,"back")]
+              , rVisited = False
+              , rItems = S.empty 
+              }
+        ,Room { rID = "best"
+              , rTitle = "Back West"
+              , rDesc = "This is the west."
+              , rExits = M.fromList [(South,"back")]
+              , rVisited = False
+              , rItems = S.fromList ["rod"]
               }
         ]
 
@@ -109,8 +126,15 @@ updateRoom r = let rid = getRoomID r in do
 	let newrooms = M.adjust (const r) rid $ gsRooms state
 	put $ state{ gsRooms = newrooms }
 
+updateInventory :: S.Set String -> StateT GameState IO ()
+updateInventory set = do
+	state <- get
+	put $ state{ gsInventory = set }
+
 getExit :: Dir -> Room -> Maybe String
-getExit dir room = 
+getExit dir room = M.lookup dir (rExits room) 
+{-	case M.member dir (rExits room) of
+		True -> Just 
 	if hasDir room dir
 	then Just $ getExit' dir $ rExits room
 	else Nothing
@@ -119,7 +143,7 @@ getExit dir room =
 		getExit' _ [] = ""
 		getExit' dir (e:es)
 			| fst e == dir = snd e
-			| otherwise = getExit' dir es
+			| otherwise = getExit' dir es-}
 
 printItemList :: String -> S.Set String -> StateT GameState IO ()
 printItemList pre set = printItemList' pre $ S.elems set
@@ -139,20 +163,34 @@ printRoom (Room { rTitle = title, rDesc = desc, rVisited = visited}) = do
 		then return ()
 		else putStrLn desc
 
-hasDir :: Room -> Dir -> Bool
-hasDir (Room {rExits = dirs}) dir = any (==dir) $ map fst dirs
+{-hasDir :: Room -> Dir -> Bool
+hasDir (Room {rExits = dirs}) dir = any (==dir) $ map fst dirs-}
 
-parseInput :: String -> Action
-parseInput "north"  = Goto North
-parseInput "south"  = Goto South
-parseInput "east"   = Goto East
-parseInput "west"   = Goto West
-parseInput "quit"   = Quit
-parseInput "look"   = Look
-parseInput "invent" = Inventory
-parseInput "pickup" = Pickup
-parseInput "drop"   = Drop
-parseInput s        = NAA s
+hasInventory :: String -> StateT GameState IO Bool
+hasInventory id = do
+	state <- get
+	return $ S.member id $ gsInventory state
+	
+itemInRoom :: String -> Room -> Bool
+itemInRoom id r = S.member id (rItems r)
+
+parseInput :: [String] -> Action
+parseInput ["north"]       = Goto North
+parseInput ["south"]       = Goto South
+parseInput ["east"]        = Goto East
+parseInput ["west"]        = Goto West
+parseInput ["quit"]        = Quit
+parseInput ["look"]        = Look Nothing
+parseInput ("look":"at":o:[])   = Look $ Just o
+parseInput ("look":o:[])   = Look $ Just o
+parseInput ["invent"]      = Inventory
+parseInput ["pickup"]      = Pickup ""
+parseInput ("pickup":o:[]) = Pickup o
+parseInput ["get"]      = Pickup ""
+parseInput ("get":o:[]) = Pickup o
+parseInput ["drop"]        = Drop ""
+parseInput ("drop":o:[])   = Drop o
+parseInput s               = NAA $ unwords s
 
 actGoto :: Dir -> StateT GameState IO ()
 actGoto dir = do
@@ -168,34 +206,74 @@ actLook = do
 	room <- getRoom $ gsInRoom state
 	lift $ printRoom room{rVisited = False}
 	printItemList "There is" $ rItems room
+	
+printItemInfo :: Item -> IO ()
+printItemInfo (Item{iDesc = desc}) = do
+	putStrLn $ desc
+
+actLookAt :: String -> StateT GameState IO ()
+actLookAt at = do
+	state <- get
+	room <- getRoom $ gsInRoom state
+	case S.member at (gsInventory state) of
+		True -> do 
+			item <- getItem at
+			lift $ printItemInfo item
+		False -> do
+			case S.member at (rItems room) of
+				True -> do 
+					item <- getItem at
+					lift $ printItemInfo item
+				False -> lift $ putStrLn $ "Don't see " ++ at ++ " here."
+	
 
 actInventory :: StateT GameState IO ()
 actInventory = do
 	state <- get
 	printItemList "You have" $ gsInventory state
 
-actPickup :: StateT GameState IO ()
-actPickup = return () --TODO
+actPickup :: String -> StateT GameState IO ()
+actPickup obj = do
+	state <- get
+	room <- getRoom $ gsInRoom state
+	case S.member obj (rItems room) of
+		True -> do
+			let newRoomItems = S.delete obj (rItems room)
+			let newInventory = S.insert obj (gsInventory state)
+			updateRoom room{ rItems = newRoomItems }
+			updateInventory newInventory
+		False -> lift $ putStrLn $ "I can't see " ++ obj ++ " here."
 
-actDrop :: StateT GameState IO ()
-actDrop = return ()   --TODO
+actDrop :: String -> StateT GameState IO ()
+actDrop obj = do
+	state <- get
+	room <- getRoom $ gsInRoom state
+	case S.member obj (gsInventory state) of
+		True -> do
+			let newInventory = S.delete obj (gsInventory state)
+			let newRoomItems = S.insert obj (rItems room)
+			updateRoom room{ rItems = newRoomItems }
+			updateInventory newInventory
+		False -> lift $ putStrLn $ "I don't have " ++ obj ++ "." 
 
 doAction :: Action -> StateT GameState IO ()
-doAction (Goto dir) = actGoto dir
-doAction Look       = actLook
-doAction Inventory  = actInventory
-doAction Pickup     = actPickup
-doAction Drop       = actDrop
-doAction Quit       = lift $ throw (ErrorCall "Quit")
-doAction (NAA str)  = lift $ putStrLn $ "Don't know how to " ++ str ++ "."
-
+doAction (Goto dir)   = actGoto dir
+doAction (Look Nothing) = actLook
+doAction (Look (Just at)) = actLookAt at
+doAction Inventory    = actInventory
+doAction (Pickup "")  = lift $ putStrLn $ "Pickup what?"
+doAction (Pickup obj) = actPickup obj
+doAction (Drop "")    = lift $ putStrLn $ "Drop what?"
+doAction (Drop obj)   = actDrop obj
+doAction Quit         = lift $ throw (ErrorCall "Quit")
+doAction (NAA str)    = lift $ putStrLn $ "Don't know how to " ++ str ++ "."
 
 playGame :: StateT GameState IO ()
 playGame = do
 	showCurrentRoom
 	lift $ putStr "> "
 	line <- lift $ getLine
-	doAction $ parseInput line
+	doAction $ parseInput $ words line
 	where
 		showCurrentRoom :: StateT GameState IO ()
 		showCurrentRoom = do
